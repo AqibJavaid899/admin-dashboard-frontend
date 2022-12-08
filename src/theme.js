674b2b32083f2c177d5649f62b01f74f2bbcc876
AqibJavaid899 <aqibjaved0910@gmail.com>
2022-12-08 23:38:10 +0500

    Fix the minor bug in the Theme Settings file

--- src/theme.js
+++ src/theme.js
@@ -58,6 +58,7 @@ const reverseDarkTokensToLightTokens = (tokensDark) => {
     }
     reverseTokens[key] = reversedObj;
   });
+  return reverseTokens;
 };
 
 export const tokensLight = reverseDarkTokensToLightTokens(tokensDark);
