8898522dcbaa1eeb48f6de8e587e225777b73909
AqibJavaid899 <aqibjaved0910@gmail.com>
2022-12-08 23:27:58 +0500

    Setup the Dark and Light Mode Theme Settings and also Configure the Redux Store for the Global State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="app">Introduction</div>;
-}
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+
+import { themeSettings } from "./theme";
+
+const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">Introduction</div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
