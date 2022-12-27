d53b4db2c7d4988ebba7f8e4556d71b2e63f3c01
AqibJavaid899 <aqibjaved0910@gmail.com>
2022-12-27 19:45:09 +0500

    Add the Router setup and also implement the basic structure of the Navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
 import { themeSettings } from "./theme";
 
 const App = () => {
@@ -11,10 +14,18 @@ const App = () => {
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="app">Introduction</div>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,67 @@
+import {
+  Menu as MenuIcon,
+  LightModeOutlined,
+  DarkModeOutlined,
+  Search,
+  SettingsOutlined,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+
+import { setMode } from "state/slices/globalSlice";
+import FlexBetween from "components/Styles/FlexBetween";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useTheme();
+
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+        border: "1px solid lightgray",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* Left Navbar */}
+        <FlexBetween gap="6px">
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="10px"
+            padding="0.1rem 1.5rem"
+            gap="3rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* Right Navbar */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {mode === "dark" ? <DarkModeOutlined /> : <LightModeOutlined />}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
